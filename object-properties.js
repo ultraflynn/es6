9997c3aa1be696364208b3a3c2b2635e799be82f
mbiggin <matthew.biggin@gmail.com>
2016-02-14 12:55:05 +0000

    Object properties

---
+++ object-properties.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let spaceship = {
+  maulus: "maulus"
+}
+
+const maulus = "maulus"
+
+let shortcut = {
+  maulus
+}
+
+console.log(JSON.stringify(shortcut))
---
+++ template-literal.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let product = {
+  name: "Broadcast Node",
+  tier: 4
+}
+
+let template = `A ${product.name} is a P${product.tier} product`
+console.log(template)
